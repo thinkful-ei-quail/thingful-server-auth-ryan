e0844250a850d3857f72a981c6a876d13e29fcf6
Ryan Whitmore <ryan@whitmorespace.com>
2020-08-27 08:47:45 -0700

    enabled logout and encrypted the seed file passwords

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,33 @@
+function requireAuth(req, res, next) {
+  const authToken = req.get('authorization') || '';
+  let basicToken;
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' });
+  } else {
+    basicToken = authToken.slice('basic '.length, authToken.length);
+  }
+
+  //What is happening here?
+  const [tokenUserName, tokenPassword] = Buffer.from(basicToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  req.app
+    .get('db')('thingful_users')
+    .where({ user_name: tokenUserName })
+    .first()
+    .then((user) => {
+      if (!user || user.password !== tokenPassword) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+      }
+      req.user = user;
+      next();
+    })
+    .catch(next);
+}
+
+module.exports = { requireAuth };
